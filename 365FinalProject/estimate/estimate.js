ba1f2382dfac6125ff56c12bbe50767da5d7479f
Javier Morales <javiermduarte70@gmail.com>
2023-12-04 16:54:50 -0500

    final for real

--- 365FinalProject/estimate/estimate.js
+++ 365FinalProject/estimate/estimate.js
@@ -1,47 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const serviceItems = document.querySelectorAll(".service-item");
+    document.addEventListener("DOMContentLoaded", function () {
+        const serviceItems = document.querySelectorAll(".service-item");
 
-    serviceItems.forEach(item => {
-        item.addEventListener("mouseenter", function () {
-            this.classList.add("hovered");
+        serviceItems.forEach(item => {
+            item.addEventListener("mouseenter", function () {
+                this.classList.add("hovered");
+            });
+
+            item.addEventListener("mouseleave", function () {
+                this.classList.remove("hovered");
+            });
         });
 
-        item.addEventListener("mouseleave", function () {
-            this.classList.remove("hovered");
+        const estimateButton = document.querySelector('button');
+
+        estimateButton.addEventListener('click', function () {
+            calculateLandscapingEstimate();
+
+            playMoneySound();
         });
     });
 
-    const estimateButton = document.querySelector('button');
+    function calculateLandscapingEstimate() {
+        const area = parseFloat(document.getElementById('area').value);
+        const plants = parseInt(document.getElementById('plants').value);
+        const services = document.getElementById('services').selectedOptions;
 
-    estimateButton.addEventListener('click', function () {
-        calculateLandscapingEstimate();
+        const baseCost = 50;
+        const areaCost = area * 0.1;
+        const plantsCost = plants * 5;
 
-        playMoneySound();
-    });
-});
+        let servicesCost = 0;
 
-function calculateLandscapingEstimate() {
-    const area = parseFloat(document.getElementById('area').value);
-    const plants = parseInt(document.getElementById('plants').value);
-    const services = document.getElementById('services').selectedOptions;
+        for (const service of services) {
+            servicesCost += 20;
+        }
 
-    const baseCost = 50;
-    const areaCost = area * 0.1;
-    const plantsCost = plants * 5;
-    
-    let servicesCost = 0;
-    
-    for (const service of services) {
-        servicesCost += 20;
+        const totalCost = baseCost + areaCost + plantsCost + servicesCost;
+
+        document.getElementById('landscaping-estimate-result').innerHTML = `Estimated Cost: $${totalCost.toFixed(2)}`;
     }
 
-    const totalCost = baseCost + areaCost + plantsCost + servicesCost;
+    function playMoneySound() {
+        const moneySound = new Audio('../estimate/money.mp3');
 
-    document.getElementById('landscaping-estimate-result').innerHTML = `Estimated Cost: $${totalCost.toFixed(2)}`;
-}
+        moneySound.play();
+    }
 
-function playMoneySound() {
-    const moneySound = new Audio('../estimate/money.mp3');
-
-    moneySound.play();
-}
+    function printPage() {
+        window.print();
+    }
