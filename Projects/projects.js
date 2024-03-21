1b97a163e7b0e9ab4ddddd6110bd516ebdbf123f
Javier Morales <javiermduarte70@gmail.com>
2024-03-21 11:57:20 -0400

    cleaned repository

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,21 +1,18 @@
-// projects.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Initialize ScrollReveal
+   
     const sr = ScrollReveal({
-        duration: 1000, // Animation duration in milliseconds
-        reset: true, // Reveal elements every time as you scroll
+        duration: 1000, 
+        reset: true,
     });
 
-    // Define the elements you want to reveal
     const projects = document.querySelectorAll('.project');
 
-    // Add the reveal effect to each project element
     projects.forEach((project, index) => {
         sr.reveal(project, {
-            delay: index * 200, // Delay the reveal of each project
-            origin: 'bottom', // Animation starts from the bottom
-            distance: '50px', // Distance of animation
+            delay: index * 200, 
+            origin: 'bottom', 
+            distance: '50px', 
         });
     });
 });
